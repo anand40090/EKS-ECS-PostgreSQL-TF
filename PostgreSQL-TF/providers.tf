4bde47d3e119715ba5abdc1bb5a39dbf134fe753
anand40090 <anand_rj91@yahoo.co.in>
2023-07-20 04:46:18 +0530

    3

---
+++ PostgreSQL-TF/providers.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  # Configuration options
+  region     = "ap-south-1"
+  access_key = "AKIAV2KW3LI7FIOWSV4Q"
+  secret_key = "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"
+}
---
+++ PostgreSQL-TF/variables.tf
@@ -0,0 +1,13 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "region" {
+  default     = "us-east-2"
+  description = "AWS region"
+}
+
+variable "db_password" {
+  description = "RDS root user password"
+  sensitive   = true
+  default = "Passw0rd!123"
+}
